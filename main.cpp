f6b383cf4b0bec0cc2f4e42f5b27a5e0ee9bf17e
Ching1zz <temirlanbajdaliev37@gmail.com>
2024-12-26 12:28:28 +0700

    работа main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include "include/polynomiac_basic.hpp"
+#include "include/MenuManager.hpp"
+MenuManager manager;
 int main(int, char**)
 {
-    //TODO : Поочерёдно показать здесь функционал классов
-    polynomial P{1, 23, 12};
-    std::cout << P << std::endl;
-    std::cout << P(1) <<std::endl;
-    P.special_out(std::cout);
+    manager.run();
+
+    return 0;
 }
