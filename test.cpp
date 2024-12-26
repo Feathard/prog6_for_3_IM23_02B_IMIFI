cbcc8964b57016d079df381ef52dc6bfa260bda7
MkFeathard <feathard2922@gmail.com>
2024-12-26 11:53:57 +0700

    Исправления

--- test.cpp
+++ test.cpp
@@ -2,17 +2,10 @@
 #include <vector>
 
 #include "include/polynomiac_basic.hpp"
-#define POLYNOMIAC_BASIC_HPP_OPERATIONS
 
 int main (int argc, char ** argv)
 {
     system("clear");
-    std::ofstream fout ("../p.csv", std::ios::app);
-    polynomial P({0xaaa, 44.4, 2});
-    std::cout << deg(P) << std::endl;
-    std::cout << P << std::endl;
-    P.special_out(std::cout);
-    P.special_out(fout);
-    fout.close();
+    
     return 0;
 }
